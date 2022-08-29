f4a834233541dd139b707da37ce27de6badafe09
DESKTOP-8H5ME3U\남상우 <deedee0905@naver.com>
2022-08-29 18:47:09 +0900

    [남상아]  - database 연결 오류가 있어서 코드 수정함(오류 처리 완료)

--- src/main/java/com/amita/springProject/ex/Ex01Controller.java
+++ src/main/java/com/amita/springProject/ex/Ex01Controller.java
@@ -1,13 +1,13 @@
 package com.amita.springProject.ex;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class Ex01Controller {
 	
-	@RequestMapping("/hello/test")
+	@GetMapping("/hello/test")
 	@ResponseBody
 	public String printoutHello() {
 		return "hello";
